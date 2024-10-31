20b0d3f80efc5b5d4f531f16fbf3e52d1f1b9b3e
Georg Plösch <georg.ploesch@gmx.at>
2024-10-31 16:05:53 +0100

    chore: Add nuget packages

---
+++ src/MarktguruApi/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+namespace MarktguruApi.Controllers
+{
+    using Asp.Versioning;
+    using Microsoft.AspNetCore.Mvc;
+    
+    [ApiController]
+    [Route("api/[controller]")]
+    [ApiVersion("1.0")]
+    public class ProductsController : ControllerBase
+    {
+        
+    }
+}
